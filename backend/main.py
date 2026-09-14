7046553851a64c2ebfb162d47133553a457d864c
Coder_Ronny@1311 <rounitmahto@gmail.com>
2026-09-14 11:10:39 +0530

    Register login route

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,15 @@
 from fastapi import FastAPI
-
-from routes.user import router as user_router
+from routes.login import router as login_router
 from routes.mandi import router as mandi_router
+from routes.user import router as user_router
 from routes.vehicle import router as vehicle_router
 
-
 app = FastAPI(title="Kisaan Mitra API")
-
 
 app.include_router(user_router)
 app.include_router(mandi_router)
 app.include_router(vehicle_router)
+app.include_router(login_router)
 
 
 @app.get("/")
